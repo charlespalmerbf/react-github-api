662e9622b6c3686f186f96d01160558250f1559c
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-25 19:41:58 +0100

    Start S3 15 Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { Fragment ,Component } from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component{
 
-  componentDidMount() {
+  state = {
 
-    
+    users: [],
+    loading: false
+
+  }
+
+  async componentDidMount() {
+
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
 
   }
 
@@ -21,7 +33,7 @@ class App extends Component{
 
         <div className="container">
 
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
 
         </div>
         
