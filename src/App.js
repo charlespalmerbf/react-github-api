35cad0e1f1e55f026aa2cd77c967a4a45e2a20d6
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-26 14:00:49 +0100

    Start S4 21 Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -10,7 +11,8 @@ class App extends Component{
   state = {
 
     users: [],
-    loading: false
+    loading: false,
+    alert: null
 
   }
 
@@ -32,6 +34,19 @@ class App extends Component{
   //Clear Users From State
   clearUsers = () => this.setState({ users: [], loading: false })
 
+  //Alert State
+  setAlert = (msg, type) => {
+
+    this.setState({ alert: { msg, type }});
+
+    setTimeout(() => {
+      
+      this.setState({ alert: null })
+
+    }, 5000);
+
+  }
+
   render() {
 
     const { users, loading } = this.state;
@@ -44,7 +59,9 @@ class App extends Component{
 
         <div className="container">
 
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false}/>
+          <Alert alert={this.state.alert} />
+
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false} setAlert={this.setAlert}/>
 
           <Users loading={loading} users={users}/>
 
