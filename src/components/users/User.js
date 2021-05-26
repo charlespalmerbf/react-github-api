7be9842882ba3f777e2927b5cdb93ce91de2166d
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-26 15:44:14 +0100

    Start S4 24 Commit

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
 
@@ -8,19 +11,76 @@ class User extends Component {
 
     }
 
+    static propTypes = {
+
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+
+    }
+
     render() {
 
-        const { name, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = this.props.user;
+        const { name, company, avatar_url, location, bio, blog, login, html_url, followers, following, public_repos, public_gists, hireable } = this.props.user;
 
         const { loading } = this.props;
 
+        if(loading) return <Spinner />;
+
         return (
 
-            <div>
-                
-                {name}
+            <Fragment>
 
-            </div>
+                <Link to='/' className='btn btn-light'> Back To Search </Link>
+
+                Hireable: {' '}
+
+                {hireable ? <i className='fas fa-check text-success'/> : <i className='fas fa-times-circle text-danger'/>}
+
+                <div className="card grid-2">
+
+                    <div className="all-center">
+
+                        <img src={avatar_url} className="round-img" alt="" style={{ width: '150px' }} />
+
+                        <h1> {name} </h1>
+
+                        <p> Location: {location} </p>
+
+                    </div>
+
+                    <div>
+
+                        {bio && (<Fragment> <h3>Bio</h3> <p> {bio} </p> </Fragment>)}
+
+                        <a href={html_url} className="btn btn-dark my-1"> Visit Github Profile </a>
+
+                        <ul>
+
+                            <li> {login && <Fragment> <strong> Username: </strong> {login} </Fragment>} </li>
+
+                            <li> {company && <Fragment> <strong> Company: </strong> {company} </Fragment>} </li>
+
+                            <li> {blog && <Fragment> <strong> Website: </strong> {blog} </Fragment>} </li>
+
+                   
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <div className="card text-center">
+
+                    <div className="badge badge-primary"> Followers: {followers} </div>
+                    <div className="badge badge-success"> Following: {following} </div>
+                    <div className="badge badge-light"> Public Repo's: {public_repos} </div>
+                    <div className="badge badge-dark"> Public Gist's: {public_gists} </div>
+
+                </div>
+
+            </Fragment>
 
         )
 
