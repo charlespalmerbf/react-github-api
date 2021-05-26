c6c95dc9fd450117320893033fc0a8860725540a
charleshpalmer <73086397+charleshpalmer@users.noreply.github.com>
2021-05-26 14:28:49 +0100

    Start S4 22 Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -53,6 +55,8 @@ class App extends Component{
 
     return (
 
+      <Router>
+
       <div className="App">
 
         <Navbar title = "Github Finder" icon = "fab fa-github"/>
@@ -61,13 +65,29 @@ class App extends Component{
 
           <Alert alert={this.state.alert} />
 
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false} setAlert={this.setAlert}/>
+          <Switch>
 
-          <Users loading={loading} users={users}/>
+              <Route exact path='/' render={props => (
+
+                <Fragment>
+
+                    <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false} setAlert={this.setAlert}/>
+
+                    <Users loading={loading} users={users}/>
+
+                </Fragment>
+
+              )} />
+
+              <Route exact path='/about' component={About} />
+
+          </Switch>
 
         </div>
         
       </div>
+
+      </Router>
 
     );
 
