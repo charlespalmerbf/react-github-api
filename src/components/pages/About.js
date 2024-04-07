1467ff87363effff57ab3a234dcff220c5efb4e2
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-04-07 17:23:08 +0100

    chore: pulling version from package.json

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
+import packageJson from "../package.json";
 
 const About = () => {
+    const version = packageJson.version;
+
     return (
         <Fragment>
             <h1>About This App</h1>
-
             <p>App to search Github Users.</p>
-
-            <p>Version: 1.0.2</p>
+            <p>Version: {version}</p>
         </Fragment>
     );
 };
