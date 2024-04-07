d61f117f8756eab76cb3787f68bfe8e3782405f1
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-04-07 18:30:49 +0100

    feat: adding more information to repo item component

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { Fragment } from "react";
+
 import PropTypes from "prop-types";
+
+import { daysAgo } from "../../utilities";
 
 const RepoItem = ({ repo }) => {
     return (
@@ -10,6 +13,18 @@ const RepoItem = ({ repo }) => {
                     {repo.name}{" "}
                 </a>
             </h3>
+            <Fragment>
+                {" "}
+                <strong> ID: </strong> {repo.id}{" "}
+            </Fragment>
+            <Fragment>
+                {" "}
+                <strong> Created: </strong> {daysAgo(repo.created_at)}{" "}
+            </Fragment>
+            <Fragment>
+                {" "}
+                <strong> Updated: </strong> {daysAgo(repo.updated_at)}{" "}
+            </Fragment>
         </div>
     );
 };
